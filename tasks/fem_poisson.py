8e464957158a429c5f1a9b1f9c783da6d41662cf
Amund Eggen Svandal <aesvandal@gmail.com>
2021-04-25 16:23:55 +0200

    Don't return locals to support pickling

--- tasks/fem_poisson.py
+++ tasks/fem_poisson.py
@@ -8,127 +8,160 @@ from refine import refine_mesh, simple_select_avg, simple_select_max
 from refinement_utilities import calculate_relative_L2_error_FEM, make_FEM_solver
 
 
-def _generate_task_5(f, u, a, b, u_text, deg=10, tol=1e-2):
+def _task_5_refinement(f, u, a, b, u_text, deg=10):
     d1 = u(a)
     d2 = u(b)
 
-    def task_5_refinement():
-        N_array = np.power(2, np.arange(3, 12), dtype=np.int32)
+    N_array = np.power(2, np.arange(3, 12), dtype=np.int32)
 
-        ndofs, (distances,) = refine_mesh(
-            solver=make_FEM_solver(
-                a=a,
-                b=b,
-                f=f,
-                d1=d1,
-                d2=d2,
-                deg=deg,
-            ),
-            param_range=N_array,
-            analytical=u,
-            calculate_distances=(calculate_relative_L2_error_FEM,),
-        )
+    ndofs, (distances,) = refine_mesh(
+        solver=make_FEM_solver(
+            a=a,
+            b=b,
+            f=f,
+            d1=d1,
+            d2=d2,
+            deg=deg,
+        ),
+        param_range=N_array,
+        analytical=u,
+        calculate_distances=(calculate_relative_L2_error_FEM,),
+    )
 
-        plt.loglog(ndofs, distances, label="$e^r_{L_2}$")
+    plt.loglog(ndofs, distances, label="$e^r_{L_2}$")
 
-        plt.plot(
-            ndofs,
-            np.divide(1, (ndofs + 1) ** 2),
-            linestyle="dashed",
-            label=r"$O\left(h^2\right)$",
-        )
+    plt.plot(
+        ndofs,
+        np.divide(1, (ndofs + 1) ** 2),
+        linestyle="dashed",
+        label=r"$O\left(h^2\right)$",
+    )
 
-        plt.suptitle("Poisson's equation FEM")
-        plt.title(fr"$u\left( x \right) = {u_text}, x \in \left[ {a}, {b} \right] $")
-        plt.xlabel("Degrees of freedom $N-1$")
-        plt.ylabel(r"Relative $L_2$ error $\frac{\|U-u\|}{\|u\|}$")
-        plt.grid()
-        plt.legend()
+    plt.suptitle("Poisson's equation FEM")
+    plt.title(fr"$u\left( x \right) = {u_text}, x \in \left[ {a}, {b} \right] $")
+    plt.xlabel("Degrees of freedom $N-1$")
+    plt.ylabel(r"Relative $L_2$ error $\frac{\|U-u\|}{\|u\|}$")
+    plt.grid()
+    plt.legend()
 
-    def task_5_afem():
-        N = 20
 
-        alpha_avg = 1.0
-        alpha_max = 0.7
+def _task_5_afem(f, u, a, b, u_text, deg=10, tol=1e-2):
+    d1 = u(a)
+    d2 = u(b)
 
-        solver_params = {
-            "N": N,
-            "f": f,
-            "u": u,
-            "a": a,
-            "b": b,
-            "d1": d1,
-            "d2": d2,
-            "tol": tol,
-            "deg": deg,
-        }
+    N = 20
 
-        x_avg, U_avg = AFEM(
-            **solver_params,
-            select_refinement=partial(simple_select_avg, alpha=alpha_avg),
-        )
+    alpha_avg = 1.0
+    alpha_max = 0.7
 
-        x_max, U_max = AFEM(
-            **solver_params,
-            select_refinement=partial(simple_select_max, alpha=alpha_avg),
-        )
+    solver_params = {
+        "N": N,
+        "f": f,
+        "u": u,
+        "a": a,
+        "b": b,
+        "d1": d1,
+        "d2": d2,
+        "tol": tol,
+        "deg": deg,
+    }
 
-        fine_x = np.linspace(a, b, max(x_avg.shape[0], x_avg.shape[0], 1000))
-        analytical = u(fine_x)
+    x_avg, U_avg = AFEM(
+        **solver_params,
+        select_refinement=partial(simple_select_avg, alpha=alpha_avg),
+    )
 
-        plt.subplot(121)
-        plt.plot(fine_x, analytical, linestyle="dashed", label="Analytical solution")
-        plt.plot(x_avg, U_avg, label="AFEM solution")
-        plt.title(fr"Select avg with $\alpha = {alpha_avg}$, ${x_avg.shape[0]}$ points")
-        plt.xlabel("$x$")
-        plt.ylabel("$y$")
-        plt.grid()
-        plt.legend()
+    x_max, U_max = AFEM(
+        **solver_params,
+        select_refinement=partial(simple_select_max, alpha=alpha_avg),
+    )
 
-        plt.subplot(122)
-        plt.plot(fine_x, analytical, linestyle="dashed", label="Analytical solution")
-        plt.plot(x_max, U_max, label="AFEM solution")
-        plt.title(fr"Select max with $\alpha = {alpha_max}$, ${x_max.shape[0]}$ points")
-        plt.xlabel("$x$")
-        plt.ylabel("$y$")
-        plt.grid()
-        plt.legend()
+    fine_x = np.linspace(a, b, max(x_avg.shape[0], x_avg.shape[0], 1000))
+    analytical = u(fine_x)
 
-        plt.suptitle(
-            "Poisson's equation AFEM "
-            fr"$u\left( x \right) = {u_text}, x \in \left[ {a}, {b} \right]$",
-        )
+    plt.subplot(121)
+    plt.plot(fine_x, analytical, linestyle="dashed", label="Analytical solution")
+    plt.plot(x_avg, U_avg, label="AFEM solution")
+    plt.title(fr"Select avg with $\alpha = {alpha_avg}$, ${x_avg.shape[0]}$ points")
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
+    plt.grid()
+    plt.legend()
 
-    return (
-        task_5_refinement,
-        task_5_afem,
+    plt.subplot(122)
+    plt.plot(fine_x, analytical, linestyle="dashed", label="Analytical solution")
+    plt.plot(x_max, U_max, label="AFEM solution")
+    plt.title(fr"Select max with $\alpha = {alpha_max}$, ${x_max.shape[0]}$ points")
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
+    plt.grid()
+    plt.legend()
+
+    plt.suptitle(
+        "Poisson's equation AFEM "
+        fr"$u\left( x \right) = {u_text}, x \in \left[ {a}, {b} \right]$",
     )
 
 
-task_5b_refinement, task_5b_afem = _generate_task_5(
-    f=lambda x: -2, u=lambda x: x ** 2, a=0, b=1, u_text="x^2"
-)
+def f_b(x):
+    return -2
 
-task_5c_refinement, task_5c_afem = _generate_task_5(
-    f=lambda x: -(40000 * x ** 2 - 200) * np.exp(-100 * x ** 2),
-    u=lambda x: np.exp(-100 * x ** 2),
-    a=-1,
-    b=1,
-    u_text=r"\exp{\left(-100 x^2\right)}",
-)
 
-task_5d_refinement, task_5d_afem = _generate_task_5(
-    f=lambda x: -(4000000 * x ** 2 - 2000) * np.exp(-1000 * x ** 2),
-    u=lambda x: np.exp(-1000 * x ** 2),
-    a=-1,
-    b=1,
-    u_text=r"\exp{\left(-1000 x^2\right)}",
-)
+def u_b(x):
+    return x ** 2
 
-task_5e_refinement, task_5e_afem = _generate_task_5(
-    f=lambda x: 2 / 9 * x ** (-4 / 3),
-    u=lambda x: x ** (2 / 3),
-    a=0,
-    b=1,
-    u_text=r"x^{\frac23}",
-)
+
+def task_5b_refinement():
+    _task_5_refinement(f=f_b, u=u_b, a=0, b=1, u_text="x^2")
+
+
+def task_5b_afem():
+    _task_5_afem(f=f_b, u=u_b, a=0, b=1, u_text="x^2")
+
+
+def f_c(x):
+    return -(40000 * x ** 2 - 200) * np.exp(-100 * x ** 2)
+
+
+def u_c(x):
+    return np.exp(-100 * x ** 2)
+
+
+def task_5c_refinement():
+    _task_5_refinement(f=f_c, u=u_c, a=-1, b=1, u_text=r"\exp{\left(-100 x^2\right)}")
+
+
+def task_5c_afem():
+    _task_5_afem(f=f_c, u=u_c, a=-1, b=1, u_text=r"\exp{\left(-100 x^2\right)}")
+
+
+def f_d(x):
+    return -(4000000 * x ** 2 - 2000) * np.exp(-1000 * x ** 2)
+
+
+def u_d(x):
+    return np.exp(-1000 * x ** 2)
+
+
+def task_5d_refinement():
+    _task_5_refinement(f=f_d, u=u_d, a=-1, b=1, u_text=r"\exp{\left(-1000 x^2\right)}")
+
+
+def task_5d_afem():
+    _task_5_afem(f=f_d, u=u_d, a=-1, b=1, u_text=r"\exp{\left(-1000 x^2\right)}")
+
+
+def f_e(x):
+    return 2 / 9 * x ** (-4 / 3)
+
+
+def u_e(x):
+    return x ** (2 / 3)
+
+
+def task_5e_refinement():
+    _task_5_refinement(f=f_e, u=u_e, a=0, b=1, u_text=r"x^{\frac23}")
+
+
+def task_5e_afem():
+    _task_5_afem(f=f_e, u=u_e, a=0, b=1, u_text=r"x^{\frac23}")
