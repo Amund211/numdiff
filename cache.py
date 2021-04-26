13951ed60d798f9de8be2ff30b237996bc6035a3
Amund Eggen Svandal <aesvandal@gmail.com>
2021-04-26 11:42:23 +0200

    Print warnings when imports fail

--- cache.py
+++ cache.py
@@ -1,9 +1,17 @@
 """
 Compatibility helper for recent features of python
 """
+
+import sys
+
 try:
     from functools import cache
 except ImportError:
+    print(
+        "WARNING: Could not import functools.cache (new in 3.9). "
+        "Using lru_cache(maxsize=None) instead.",
+        file=sys.stderr,
+    )
     from functools import lru_cache
 
     def cache(f):
@@ -13,6 +21,11 @@ except ImportError:
 try:
     from functools import cached_property
 except ImportError:
+    print(
+        "WARNING: Could not import functools.cached_property (new in 3.8). "
+        "Using property(cache(f)) instead.",
+        file=sys.stderr,
+    )
 
     def cached_property(f):
         return property(cache(f))
